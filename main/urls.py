3136c90eb2cff51e0a28d35e1ca6e82223985124
Felipe Rios <feliperios@Felipes-MacBook-Pro.local>
2017-05-16 16:32:46 -0400

    Fix urls file

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
 from main import views
-from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view()),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
